896ffd577c54ffedc03890dcd1d0b10a570e6020
venskoA <venskoandrey@gmail.com>
2022-05-25 20:54:08 +0300

    second commit

---
+++ VKSwiftUI/VKSwiftUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  VKSwiftUIApp.swift
+//  VKSwiftUI
+//
+//  Created by Andrey Vensko on 25.05.22.
+//
+
+import SwiftUI
+
+@main
+struct VKSwiftUIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
